508d846e7a05573c69904545fc5543804b96c327
lmihelac <luka.mihelac7@gmail.com>
2022-06-10 22:40:33 +0200

    add another date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,11 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date}</div>
+      <div>
+        <div>{props.date.toLocaleString("en-US", {})}</div> //month
+        <div></div> //year
+        <div></div> //day
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
