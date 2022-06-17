e6eb9f0c203722479dc0d9c4b02453b70641e7a2
lmihelac <luka.mihelac7@gmail.com>
2022-06-17 23:02:04 +0200

    add click handler

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,13 +3,18 @@ import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    alert("works");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
+      <div onDoubleClick={clickHandler} className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>TEst brt</button>
     </Card>
   );
 };
